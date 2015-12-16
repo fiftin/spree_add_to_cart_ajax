c873818cff0da5fddcef82d6e2592c8333b7417b
Denis Gukov <denguk@gmail.com>
2015-12-16 16:40:03 +0500

    chore: remove link to jquery.form from js file

--- app/assets/javascripts/spree/frontend/spree_add_to_cart_ajax.js
+++ app/assets/javascripts/spree/frontend/spree_add_to_cart_ajax.js
@@ -1,6 +1,5 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
-//= require jquery.form
 
 (function($) {
 
